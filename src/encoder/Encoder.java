fad75b4ccc18c93dd645b7bf73d4d959afa24bcc
Govind-Girish <govindgirish@live.com>
2015-04-06 15:36:25 -0500

    Change #1
    Added Encoder Constructor and methods

---
+++ src/encoder/Encoder.java
@@ -0,0 +1,23 @@
+package encoder;
+
+import java.util.List;
+
+public class Encoder 
+{
+	public Encoder (String filename)
+	{
+		
+	}
+	
+	public void write(String plaintext)
+	{
+		
+	}
+	
+	public List<String> read()
+	{
+		
+		
+		return null;
+	}
+}
--- src/encoder/Main.java
+++
@@ -1,5 +0,0 @@
-package encoder;
-
-public class Main {
-
-}
