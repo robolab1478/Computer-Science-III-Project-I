41385ccb9d43309105f273d63a7e28aeef01a16c
Vikas <robolab1478@gmail.com>
2015-04-06 15:07:46 -0500

    Added Driver package
    :1234:

---
+++ src/driver/Main.java
@@ -0,0 +1,11 @@
+package driver;
+
+import javax.swing.SwingUtilities;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		SwingUtilities.invokeLater(new Runnable(){public void run(){new frontend.Main();}});
+	}
+}
