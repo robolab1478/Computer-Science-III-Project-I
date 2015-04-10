75ebb41ffdefba138253ad89b91705b7994622ba
Vikas <robolab1478@gmail.com>
2015-04-10 14:52:51 -0500

    Fixed Rohan's backend
    QQ

--- src/backend/BackEnd.java
+++ src/backend/BackEnd.java
@@ -1,24 +1,16 @@
 package backend;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-<<<<<<< HEAD
-import encoder.Encoder;
-
-=======
->>>>>>> origin/master
-public class BackEnd {
+public class BackEnd
+{
 
 
 	public List<String> getMessages(String username) throws IOException
 	{
-<<<<<<< HEAD
 			
 	//	Encoder encoder(username);
 	//	return encoder.read();
@@ -56,37 +48,26 @@ public class BackEnd {
 		return backupMessage;
 
 	}*/
-=======
-		Encoder encoder(username);
-		return encoder.read();
+		return new encoder.Encoder(username+".crpt").read();
 	}
->>>>>>> origin/master
 
 	public void addMessage(String username, String msg)
 	{
 		//adds message to the encoded file(s)
-<<<<<<< HEAD
 		//if file exists; add message
 		//if file does not exist; make file and add message
 		
-		Encoder encoder(username);
-		encoder.write(msg);
-
-=======
+		//Encoder encoder(username);
+		///encoder.write(msg);
 		
-		return new encoder(username).write(msg);
->>>>>>> origin/master
+		new encoder.Encoder(username).write(msg);
 	}
 
 	public List<String> getUsers()
 	{
 		//returns a list of all users who have submitted a message
-<<<<<<< HEAD
 		
 		File f = new File("userMessages");
-=======
-	File f = new File("userMessages");
->>>>>>> origin/master
 		File[] showFiles = f.listFiles();
 		ArrayList<String> users = new ArrayList<String>();
 		String extension = ".txt";
@@ -95,22 +76,10 @@ public class BackEnd {
 			String filename = l.getName();
 			String ext = filename.substring(filename.length() - extension.length());
 			if(ext.equals(extension))
-			{
 				users.add(filename);
-			}
 		}
 		
 		
 		return users;
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
 	}
-
-	public static void main(String[] args)
-	{
-
-	}
-
 }
